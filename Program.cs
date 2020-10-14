757be0f9297247e81e1f7632343437bde9d3857e
Stephan Schumacher aka Worthy Alpaca <stephanxxschumacher@gmail.com>
2020-10-14 17:21:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+// Namespace
+namespace Numberguesser
+{
+    // Main Class
+    class Program
+    {
+        // Entry point
+        static void Main(string[] args)
+        {
+        }
+    }
+}
